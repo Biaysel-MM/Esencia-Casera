a58d0b3777ac7bc9aace27c6803edccba30c586e
Biaysel-MM <biayselminyety001@gmail.com>
2026-01-08 16:14:57 -0400

    solucionando un error de nombres de variables

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-      define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY)
-  },
     resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
